827cf5a2d90e60f429c85fca1f47f25f2a31c03b
Aditya Saxena <144054604+Adityasaxena1@users.noreply.github.com>
2024-08-14 22:58:46 +0530

    emptyCard.jsx

--- app/components/emptyCard.jsx
+++ app/components/emptyCard.jsx
@@ -1,18 +1,1 @@
 
-
-export default function EmptyCard(props) {
-    return (
-        <Card>
-        <EmptyState
-          heading="Schedule Your Products Delivery"
-          action={{
-            content: "Schedule Delivery",
-            onAction: props.handle,
-          }}
-          image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
-        >
-          <p>Delivery Schedule is just one click away.</p>
-        </EmptyState>
-      </Card>
-    )
-}
